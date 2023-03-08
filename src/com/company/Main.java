09507494c015e733e03f3b51e456b81b3700a1ee
Overlord7667 <aspiaro@gmail.com>
2023-03-08 19:45:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,148 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Lesson #1");
+//        System.out.println("Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Неверно'.\n");
+//        int a = 10;
+//        if (a == 10) {
+//            System.out.println("Верно");
+//        }
+//        else {
+//            System.out.println("Неверно");
+//        }
+
+//        System.out.println("Lesson #2");
+//        System.out.println("В переменной min лежит число от 0 до 59. Определите в какую четверть часа попадает это число" +
+//                " (в первую, вторую, третью или четвертую).\n");
+//        int main = 40;
+//        if (main >= 0 && main <= 14){
+//            System.out.println("Это первая четверть часа");
+//        }
+//        if (main >= 15 && main <= 29) {
+//            System.out.println("Это вторая четверть часа");
+//        }
+//        if (main >= 30 && main <= 44) {
+//            System.out.println("Это третья четверть часа");
+//        }
+//        if (main >= 45 && main <= 59) {
+//            System.out.println("Это четвёртая четверть часа");
+//        }
+
+//        System.out.println("Lesson #3");
+//        System.out.println("Если переменная a больше нуля, то выведите 'Верно', иначе выведите 'Неверно'." +
+//                " Проверьте работу скрипта при a, равном 1, 0, -3.\n");
+//        int a = 1;
+//        if (a < 0){
+//            System.out.println("Неверно");
+//        } else if (a==0) {
+//            System.out.println(" a = 0");
+//        }else {
+//            System.out.println("Верно");
+//        }
+
+//        System.out.println("Lesson #4");
+//        System.out.println("Если переменная a меньше нуля, то выведите 'Верно', иначе выведите 'Неверно'." +
+//                " Проверьте работу скрипта при a, равном 1, 0, -3.\n");
+//        int a = -1;
+//        if (a > 0){
+//            System.out.println("Неверно");
+//        } else if (a==0) {
+//            System.out.println(" a = 0");
+//        }else {
+//            System.out.println("Верно");
+//        }
+
+//        System.out.println("Lesson #5");
+//        System.out.println("Если переменная a больше или равна нулю, то выведите 'Верно', иначе выведите 'Неверно'." +
+//                " Проверьте работу скрипта при a, равном 1, 0, -3.\n");
+//        int a = 1;
+//        if (a >= 0){
+//            System.out.println("Верно");
+//        }else {
+//            System.out.println("Неверно");
+//        }
+
+//        System.out.println("Lesson #6");
+//        System.out.println("Если переменная a меньше или равна нулю, то выведите 'Верно', иначе выведите 'Неверно'." +
+//                " Проверьте работу скрипта при a, равном 1, 0, -3.\n");
+//        int a = 1;
+//        if (a <= 0){
+//            System.out.println("Верно");
+//        }else {
+//            System.out.println("Неверно");
+//        }
+
+//        System.out.println("Lesson #7");
+//        System.out.println("Если переменная a не равна нулю, то выведите 'Верно', иначе выведите 'Неверно'." +
+//                " Проверьте работу скрипта при a, равном 1, 0, -3.\n");
+//        int a = -1;
+//        if (a != 0){
+//            System.out.println("Верно");
+//        }else {
+//            System.out.println("Неверно");
+//        }
+
+//        System.out.println("Lesson #8");
+//        System.out.println("Если переменная a равна 'test', то выведите 'Верно', иначе выведите 'Неверно'." +
+//                " Проверьте работу скрипта при a, равном 'test', 'тест', 3.\n");
+//        String test;
+//        String a;
+//        a = "test";
+//        test = "test";
+//        if (test == a) {
+//            System.out.println("Верно");
+//        }else {
+//            System.out.println("Неверно");
+//        }
+
+//        System.out.println("Lesson #9");
+//        System.out.println("Если переменная a равна '1' и по значению и по типу, то выведите 'Верно'," +
+//                " иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном '1', 1, 3.\n");
+//        int a = 1;
+//        int b = 1;
+//        if (a == b) {
+//
+//        }
+
+//        System.out.println("Lesson #10");
+//        System.out.println("Есть три числа (a,b,c) c помощью if else  выведите максимум и минимум\n");
+//        int a = 23;
+//        int b = 2;
+//        int c = 13;
+//        if (a>b && a>c){
+//            System.out.println("a max");
+//        }else if (a<b && a<c){
+//            System.out.println("a min");
+//        }
+//        if (b>a && b>c){
+//            System.out.println("b max");
+//        }else if (b<a && b<c){
+//            System.out.println("b min");
+//        }
+//        if (c>a && c>b){
+//            System.out.println("c max");
+//        }else if (c<a && c<b){
+//            System.out.println("c min");
+//        }
+
+//        System.out.println("Lesson #11");
+//        System.out.println("Есть число (a) вывести  положительное или отрицательное\n");
+//        int a = 4;
+//        if (a > 0){
+//            System.out.println("Число положительное");
+//        }else {
+//            System.out.println("Число отрицательное");
+//        }
+
+//        System.out.println("Lesson #12");
+//        System.out.println("Есть число (a) вывести  четное не четное\n");
+//        int a = 9;
+//        if ((a%2) == 0) {
+//            System.out.println("Чётное");
+//        }else {
+//            System.out.println("Не чётное");
+//        }
+    }
+}
